b9fe8284102b168616a3ef6a202dcef933177465
Scott Hutley <scotthutley1@comcast.net>
2021-06-09 16:23:28 -0700

    Add vowel logic

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,12 +1,12 @@
 function pigLatin(text) {
   const vowels = ["a","e","i","o","u"];
   const textArray = text.split(" ");
-  let output="";
+  let output = "";
   textArray.forEach(function(element) {
     for (let i=0; i < vowels.length; i++) {
-      if (vowels[i] === element.toLowerCase().slice(0,1)) {
-        output = output.concat(element.toLowerCase()+"way ");
-      }
+      if (vowels[i] = element.toLowerCase().slice(0,1)) {
+        output = output+element+"way ";
+      } 
     }
   });
   return output.slice(0,output.length-1);
